3647bbc2810618a0ea3f89fcf8408050f9ad5c19
Z1aht <130074959+Z1aht@users.noreply.github.com>
2023-10-16 10:01:20 -0300

    att1

--- aluramidi-curso-aula2/main.js
+++ aluramidi-curso-aula2/main.js
@@ -2,9 +2,10 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-const = listadeTeclasdocument.querySelectorAll('.tecla')
+const = listadeTeclasdocument.querySelectorAll('.tecla');
 
 
 while () {
-    listadeTeclas[0].onclick = tocaSomPom
+    listadeTeclas[0].onclick = tocaSomPom;
+    contador = contador+1;
 }
