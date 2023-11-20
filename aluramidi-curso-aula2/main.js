0385daefd44e4f4567ac3c87e1d0d3a9133dfd7f
Z1aht <130074959+Z1aht@users.noreply.github.com>
2023-11-20 10:00:59 -0300

    oie

--- aluramidi-curso-aula2/main.js
+++ aluramidi-curso-aula2/main.js
@@ -8,15 +8,23 @@ const = listadeTeclasdocument.querySelectorAll('.tecla');
 
 
 for (let contador = 0; contador < listaDeTecla.lenght; contador++) {
+
     const.instrumento = listadeTeclas[contador].classList[1];
     listadeTeclas[contador].onclick = tocaSom;
     contador = contador+1;
 
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
     tecla.onKeydown = function(evento){
-        if(evento.code == 'enter')
-    tecla.classlist.add('ativa')
-}
-tecla.onkeyup(){
-    tecla.classlist.remove('ativa')
-}
+
+        if(evento.code == 'Enter')
+        tecla.classlist.add('ativa')
+        }
+        }
+
+    tecla.onkeyup(){
+        tecla.classlist.remove('ativa')
+        }
 }
